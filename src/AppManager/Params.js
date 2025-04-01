f465855189979ac45774bbdbb24a4daf83be6f5d
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-01 17:46:14 +0300

    first commit

---
+++ src/AppManager/Params.js
@@ -0,0 +1,5 @@
+export const keyPush = 'e55cf5a1-abfe-40f7-aedd-1f00781231f8';
+export const appID = 'id6743625816';
+export const keyApps = 'T9h7tLijQjDdZXpvgxh44L';
+export const targetDate = '2025-04-04';
+export const bodyLin = 'https://glorious-epic-cheer.space/sDmvdw4z';
